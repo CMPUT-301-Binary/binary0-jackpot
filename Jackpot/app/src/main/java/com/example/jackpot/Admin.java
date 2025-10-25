92e2ab7558898cb303ecd1794e58509dc5e0b561
HRY47 <raffieip@ualberta.ca>
2025-10-25 14:02:17 -0600

    Adding skeleton code for Admin, Entrant,Image,User.

---
+++ Jackpot/app/src/main/java/com/example/jackpot/Admin.java
@@ -0,0 +1,5 @@
+package com.example.jackpot;
+
+public class Admin extends User {
+
+}
---
+++ Jackpot/app/src/main/java/com/example/jackpot/Image.java
@@ -0,0 +1,39 @@
+package com.example.jackpot;
+
+import java.net.URL;
+import java.util.UUID;
+
+public abstract class Image {
+    private UUID imageID;
+    //private URL url;
+    private UUID uploadedBy;
+
+    public Image(UUID uploadedBy) {
+        this.imageID = UUID.randomUUID();
+        this.uploadedBy = uploadedBy;
+    }
+
+    public UUID getImageID() {
+        return imageID;
+    }
+
+    public UUID getUploadedBy() {
+        return uploadedBy;
+    }
+
+    public void setImageID(UUID imageID) {
+        this.imageID = imageID;
+    }
+
+    public void setUploadedBy(UUID uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
+
+    public void deleteImage(UUID imageID) {
+        if (imageID == this.imageID) {
+            this.imageID = null;
+            this.uploadedBy = null;
+        }
+    }
+
+}
