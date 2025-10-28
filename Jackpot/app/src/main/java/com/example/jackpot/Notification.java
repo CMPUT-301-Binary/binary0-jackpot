3bd5907af6ebf1a067ee13812d8cd7299f74cc01
Ibrahim Mansoor <171342825+ibmansoor@users.noreply.github.com>
2025-10-28 14:37:53 -0600

    Complete skeleton of notification class

--- Jackpot/app/src/main/java/com/example/jackpot/Notification.java
+++ Jackpot/app/src/main/java/com/example/jackpot/Notification.java
@@ -7,13 +7,86 @@ public class Notification {
     private UUID notificationID;
     private UUID recipientID;
     private UUID eventID;
-
-//    private String notificationType;
-
+    //    private String notificationType;
     private String payload;
     private Instant sentAt;
     private boolean deliverStatus;
     private String providerMsgID;
     private String error;
+
+
+    public UUID getNotificationID() {
+        return notificationID;
+    }
+
+    public void setNotificationID(UUID notificationID) {
+        this.notificationID = notificationID;
+    }
+
+    public UUID getRecipientID() {
+        return recipientID;
+    }
+
+    public void setRecipientID(UUID recipientID) {
+        this.recipientID = recipientID;
+    }
+
+    public UUID getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(UUID eventID) {
+        this.eventID = eventID;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    public Instant getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(Instant sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public boolean isDeliverStatus() {
+        return deliverStatus;
+    }
+
+    public void setDeliverStatus(boolean deliverStatus) {
+        this.deliverStatus = deliverStatus;
+    }
+
+    public String getProviderMsgID() {
+        return providerMsgID;
+    }
+
+    public void setProviderMsgID(String providerMsgID) {
+        this.providerMsgID = providerMsgID;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public void queue(){};
+
+    public void markSent(Instant at, String providerMsgID){};
+
+    public void markFailed(Instant at, String error){};
+
+    public boolean isDeliverable(){
+        return true; // placeholder
+    };
 
 }
