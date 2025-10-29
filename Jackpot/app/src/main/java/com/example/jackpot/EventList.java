39f0446e57c80de0d8d7851f5eb0726cabb6d50c
parallelXYZ <lnone67@gmail.com>
2025-10-29 13:39:50 -0600

    added EventList.java

---
+++ Jackpot/app/src/main/java/com/example/jackpot/EventList.java
@@ -0,0 +1,43 @@
+package com.example.jackpot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventList {
+    private ArrayList<Event> events;
+
+    public EventList(ArrayList<Event> events) {
+        this.events = events;
+    }
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+    public void removeEvent(Event event) {
+        events.remove(event);
+    }
+    public List<Event> getEvents() {
+        return events;
+    }
+    public int countEvents() {
+        return events.size();
+    }
+    public Event getEvent(int index) {
+        return events.get(index);
+    }
+    public boolean containsEvent(Event event) {
+        return events.contains(event);
+    }
+    public void clearEvents() {
+        events.clear();
+    }
+    public int indexOfEvent(Event event) {
+        return events.indexOf(event);
+    }
+    public int lastIndexOfEvent(Event event) {
+        return events.lastIndexOf(event);
+    }
+    // sorting method if we must sort events
+//    public void sortEvents() {
+//        events.sort(null);
+//    }
+}
