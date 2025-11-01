3efd50067965c5ea1155df7b5014bf2ff9c3cfc5
parallelXYZ <lnone67@gmail.com>
2025-11-01 11:47:11 -0600

    added activities

---
+++ Jackpot/app/src/main/java/com/example/jackpot/activities/ui/AdminActivity.java
@@ -0,0 +1,17 @@
+package com.example.jackpot.activities.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jackpot.R;
+
+public class AdminActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_admin);
+
+
+    }
+}
