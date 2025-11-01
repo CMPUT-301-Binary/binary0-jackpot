40965a82919102c2de9e499155547b7c5ac9b267
stellarhesk <jessemuotoh@gmail.com>
2025-11-01 11:09:23 -0600

    Modified User, fixed the children classes

--- Jackpot/app/src/main/java/com/example/jackpot/User.java
+++ Jackpot/app/src/main/java/com/example/jackpot/User.java
@@ -2,29 +2,27 @@ package com.example.jackpot;
 
 import java.util.UUID;
 
-public class User {
-    private final UUID id;
-    private String name;
-    private Role role;
-    private String email;
-    private String phone; //Could be NULL
-    private String password;
-    private String deviceID;
 
-    public User(String name, Role role, String email, String phone, String password, String deviceID) {
-        this.id = java.util.UUID.randomUUID();
+
+public class User {
+    protected final UUID id;
+    protected String name;
+    protected Role role;
+    protected String email;
+    protected String phone; //Could be NULL
+    protected String password;
+    protected String notificationPreferences;
+    protected Device device;
+
+    public User(UUID id, String name, Role role, String email, String phone, String password, String notificationPreferences, Device device) {
+        this.id = id;
         this.name = name;
         this.role = role;
         this.email = email;
         this.phone = phone;
         this.password = password;
-        this.deviceID = deviceID;
-    }
-
-    public enum Role {
-        ENTRANT,
-        ORGANIZER,
-        ADMIN
+        this.notificationPreferences = notificationPreferences;
+        this.device = device;
     }
 
     public UUID getId() {
@@ -35,47 +33,61 @@ public class User {
         return name;
     }
 
-    public Role getRole() {
-        return role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getDeviceID() {
-        return deviceID;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
     }
 
     public void setRole(Role role) {
         this.role = role;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public void setDeviceID(String deviceID) {
-        this.deviceID = deviceID;
+    public String getNotificationPreferences() {
+        return notificationPreferences;
+    }
+
+    public void setNotificationPreferences(String notificationPreferences) {
+        this.notificationPreferences = notificationPreferences;
+    }
+
+    public Device getDevice() {
+        return device;
+    }
+
+    public void setDevice(Device device) {
+        this.device = device;
+    }
+
+    public enum Role {
+        ENTRANT,
+        ORGANIZER,
+        ADMIN
     }
 }
