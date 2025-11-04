a0bbbc77bc3428fa29217cd2649f96a0880b4aa2
HRY47 <raffieip@ualberta.ca>
2025-11-04 16:53:15 -0700

    adding Admin, Organizer bottom bar and pages.

--- Jackpot/app/src/main/java/com/example/jackpot/ui/home/HomeFragment.java
+++ Jackpot/app/src/main/java/com/example/jackpot/ui/home/HomeFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.jackpot.R;
+import com.example.jackpot.User;
 
 public class HomeFragment extends Fragment {
 
@@ -22,9 +23,32 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_home_entrant, container, false);
+        //String roleString = requireActivity().getIntent().getStringExtra("USER_ROLE");
+        //User.Role currentRole = User.Role.valueOf(roleString);
 
+        String roleName = getArguments() != null ? getArguments().getString("role") : "ENTRANT";
+        User.Role role = User.Role.valueOf(roleName);
 
+        if (roleName != null) {
+            role = User.Role.valueOf(roleName);
+        } else {
+            // Default to ENTRANT
+            role = User.Role.ENTRANT;
+        }
+        role = User.Role.ORGANIZER; // TESTINGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+        // Inflate correct home layout
+        View root;
+        switch (role) {
+            case ORGANIZER:
+                root = inflater.inflate(R.layout.fragment_home_organizer, container, false);
+                break;
+            case ADMIN:
+                root = inflater.inflate(R.layout.fragment_home_admin, container, false);
+                break;
+            default:
+                root = inflater.inflate(R.layout.fragment_home_entrant, container, false);
+                break;
+        }
 
         return root;
     }
--- Jackpot/app/src/main/java/com/example/jackpot/ui/notification/NotificationFragment.java
+++ Jackpot/app/src/main/java/com/example/jackpot/ui/notification/NotificationFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.jackpot.R;
+import com.example.jackpot.User;
 
 public class NotificationFragment extends Fragment {
     @Nullable
@@ -16,10 +17,35 @@ public class NotificationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_notification_entrant, container, false);
-        TextView tv = new TextView(getContext());
-        tv.setText("Notifications Page");
-        ((ViewGroup) root).addView(tv);
+//        View root = inflater.inflate(R.layout.fragment_notification_entrant, container, false);
+//        TextView tv = new TextView(getContext());
+//        tv.setText("Notifications Page");
+//        ((ViewGroup) root).addView(tv);
+//        return root;
+        String roleName = getArguments() != null ? getArguments().getString("role") : "ENTRANT";
+        User.Role role = User.Role.valueOf(roleName);
+
+        if (roleName != null) {
+            role = User.Role.valueOf(roleName);
+        } else {
+            // Default to ENTRANT or whatever makes sense
+            role = User.Role.ENTRANT;
+        }
+        role = User.Role.ORGANIZER; // TESTINGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+        // Inflate correct home layout
+        View root;
+        switch (role) {
+            case ORGANIZER:
+                root = inflater.inflate(R.layout.fragment_notification_organizer, container, false);
+                break;
+            case ADMIN:
+                root = inflater.inflate(R.layout.fragment_notification_admin, container, false);
+                break;
+            default:
+                root = inflater.inflate(R.layout.fragment_notification_entrant, container, false);
+                break;
+        }
+
         return root;
     }
 }
