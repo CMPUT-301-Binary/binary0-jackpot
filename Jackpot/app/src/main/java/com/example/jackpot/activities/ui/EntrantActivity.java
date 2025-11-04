e666ed2cffe2b96b912bb430f06123cc4c087fb2
parallelXYZ <lnone67@gmail.com>
2025-11-03 17:16:21 -0700

    added a function in EntrantActivity to query the db for events matching a filter

--- Jackpot/app/src/main/java/com/example/jackpot/activities/ui/EntrantActivity.java
+++ Jackpot/app/src/main/java/com/example/jackpot/activities/ui/EntrantActivity.java
@@ -2,15 +2,50 @@ package com.example.jackpot.activities.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.jackpot.Event;
 import com.example.jackpot.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class EntrantActivity extends AppCompatActivity {
+    private DatabaseReference db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_entrant);
 
+    }
+    public ArrayList<Event> queryEvents(String param, String value, DatabaseReference db) {
+        ArrayList<Event> results = new ArrayList<>();
+        Query query = db.orderByChild(param).equalTo(value);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        Event event = dataSnapshot.getValue(Event.class);
+                        results.add(event);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                error.toException().printStackTrace();
+                // handle error
+                // int errorCode = error.getCode();
+                // act accordingly
+            }
+        });
+        return results;
     }
 }
