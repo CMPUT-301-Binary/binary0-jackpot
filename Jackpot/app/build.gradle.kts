538627783ff71a86cc2ca58f7523b5989b139b03
Ibrahim Mansoor <171342825+ibmansoor@users.noreply.github.com>
2025-11-05 15:13:46 -0700

    Add material implementation

--- Jackpot/app/build.gradle.kts
+++ Jackpot/app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.firebase:firebase-firestore:24.10.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
